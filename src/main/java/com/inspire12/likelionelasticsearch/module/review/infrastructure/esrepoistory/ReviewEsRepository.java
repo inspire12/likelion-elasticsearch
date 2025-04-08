408377139508451436324bb7cc3576a8aa4d9fdb
inspire12 <ox4443@naver.com>
2025-04-08 14:31:54 +0900

    feat(es-spring-complex-start-practice): 코드 샘플 추가

--- src/main/java/com/inspire12/likelionelasticsearch/module/review/infrastructure/esrepoistory/ReviewEsRepository.java
+++ src/main/java/com/inspire12/likelionelasticsearch/module/review/infrastructure/esrepoistory/ReviewEsRepository.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ReviewEsRepository extends ElasticsearchRepository<ReviewDocument, Long> {
+public interface ReviewEsRepository extends ElasticsearchRepository<ReviewDocument, Long>
+        // TODO 확인, JPA 할 때 QueryDSL 처럼 RestClient 같은 raw한 형태로 api를 날리는 부분을 통합
+        , ReviewTemplateEsRepository {
     Page<ReviewDocument> findAllByCustomerId(Long customerId, Pageable pageable);
 }
