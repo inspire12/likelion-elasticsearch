86e48b866ef637ab27182c7f9c5b548ab9d251e9
inspire12 <ox4443@naver.com>
2025-04-08 09:16:11 +0900

    feat(es-input): dynamicField 추가

--- src/main/java/com/inspire12/likelionelasticsearch/module/review/application/dto/request/ReviewRequest.java
+++ src/main/java/com/inspire12/likelionelasticsearch/module/review/application/dto/request/ReviewRequest.java
@@ -1,8 +1,13 @@
 package com.inspire12.likelionelasticsearch.module.review.application.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @NoArgsConstructor
@@ -13,4 +18,7 @@ public class ReviewRequest {
     private Long orderId;
     private Long storeId;
     private Integer rating;
+
+    @JsonAnySetter
+    Map<String, Object> dynamicField = new HashMap<>();
 }
