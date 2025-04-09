f3e9368d948902f3da98c912a778b320b8202583
inspire12 <ox4443@naver.com>
2025-04-09 10:42:32 +0900

    feat(es-completion-practice): practice

--- src/main/java/com/inspire12/likelionelasticsearch/module/review/infrastructure/document/ReviewDocument.java
+++ src/main/java/com/inspire12/likelionelasticsearch/module/review/infrastructure/document/ReviewDocument.java
@@ -2,11 +2,9 @@ package com.inspire12.likelionelasticsearch.module.review.infrastructure.documen
 
 
 import lombok.*;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
-import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.*;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.core.suggest.Completion;
 
 import java.time.LocalDateTime;
 
@@ -39,6 +37,9 @@ public class ReviewDocument {
 
     @Field(type = FieldType.Keyword)
     private String sentiment;
+
+    @CompletionField
+    private Completion suggest;
 
     @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
     //@Field(type = FieldType.Date, format = {}, pattern = "yyyy-MM-dd'T'HH:mm:ss")
